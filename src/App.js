13f6506e5cbb9db9e140db5c57380b9d1b3b7801
Angela Clarke <angeladoust@hotmail.com>
2022-08-02 20:02:07 +1000

    code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
+import Nav from "./components/Nav"
+
+
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/Contactpage';
 
 function App() {
   return (
-    <div className="App">
-       Hello
-    </div>
+    <Router>
+      <div>
+        <Nav />
+      <Switch>
+      <Route path="/">
+          <HomePage />
+        </Route>
+        <Route path="/contact">
+          <ContactPage />
+        </Route>
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+      </Switch>
+      </div>
+    </Router>
   );
 }
 
